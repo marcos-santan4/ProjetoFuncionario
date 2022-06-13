4dddd52bb053e4129cd5545dd8902d92be2c8312
Marcos Santana <ssmarcosantana@gmail.com>
2022-06-12 21:35:48 -0300

    Adicionado Frame e modificado o projetoFuncionario

---
+++ src/Frame.java
@@ -0,0 +1,8 @@
+import javax.swing.JFrame;
+
+public class Frame extends JFrame {
+    
+    public Frame() {
+        super("Cadastro de Funcionarios");
+    }
+}
--- src/projetoFuncionario.java
+++ src/projetoFuncionario.java
@@ -1,7 +1,14 @@
+import javax.swing.JFrame;
 public class projetoFuncionario {
     public static void main(String[] args) {
-        ColetaDados cd = new ColetaDados();
-        cd.menu();
+        //ColetaDados cd = new ColetaDados();
+        //cd.menu();
         //comentario para teste do git
+
+        Frame frame = new Frame();
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 250);
+        frame.setVisible(true);
     }
 }
